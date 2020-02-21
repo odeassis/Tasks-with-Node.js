b36bdb480604e316983cf12ba2dd58e7bbef479b
fdAssis <f.assis@acad.ifma.edu.br>
2020-02-21 08:42:33 -0300

    add routes in index

--- index.js
+++ index.js
@@ -3,8 +3,40 @@ const express = require('express');
 const server = express();
 server.use(express.json());
 
-server.get('/', (req, res) => {
-  return res.json({message : "Hello Word!"});
+const projects = [];
+
+// Rota para listar todos os projetos
+server.get('/projects', (req, res) => {
+  return res.json(projects);
+});
+
+// Rota para adicionar um projeto
+server.post('/projects', (req, res) => {
+  const { id , title } = req.body;
+
+  const project = {
+    id,
+    title,
+    tasks:[]
+  };
+  
+  projects.push(project);
+  
+  return res.json(projects);
+
+});
+
+// Rota para alterar o titulo de uma tarefa
+server.post('/projects/:id', (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(proj => proj.id == id);
+
+  project.title = title;
+
+  return res.json(project);
+
 });
 
 server.listen(3030);
